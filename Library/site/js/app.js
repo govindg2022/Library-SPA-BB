18ebdfbc6e0a92a38ad3c834dcbf8b56ddf5b3d6
Govind Gupta <govindgupta@govindguptasmbp.home>
2014-05-24 21:51:40 -0400

    My First Commit of Library Application After completion

---
+++ Library/site/js/app.js
@@ -0,0 +1,20 @@
+var app = app || {};
+$(function(){
+	/* Sample data has been removed as we are taking the input from server
+  var books =[
+  		      { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford',
+              releaseDate: '2008', keywords: 'JavaScript Programming' },
+            { title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw',
+              releaseDate: '2012', keywords: 'CoffeeScript Programming' },
+            { title: 'Scala for the Impatient', author: 'Cay S. Horstmann',
+              releaseDate: '2012', keywords: 'Scala Programming' },
+            { title: 'American Psycho', author: 'Bret Easton Ellis',
+              releaseDate: '1991', keywords: 'Novel Splatter' },
+            { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke',
+              releaseDate: '2011', keywords: 'JavaScript Programming' }
+    ];*/
+
+           $( '#releaseDate' ).datepicker();
+           new app.LibraryView(books);
+
+});
---
+++ Library/site/js/views/library.js
@@ -0,0 +1,56 @@
+var app = app || {};
+app.LibraryView = Backbone.View.extend({
+	el: "#books",
+
+	initialize: function(){
+		this.collection = new app.Library();
+		this.collection.fetch({reset: true}); //NEW
+		this.render();
+		this.listenTo( this.collection, 'add', this.renderBook );
+		this.listenTo( this.collection, "reset", this.render); //NEW
+	},
+
+	events: {
+		"click #add" :"addBook"
+	},
+
+	render: function(){
+		this.collection.each(function (item){
+			this.renderBook( item );
+		}, this );
+	},
+
+	renderBook: function(item) {
+		var bookView = new app.BookView({
+		model: item
+	});
+		this.$el.append( bookView.render().el );
+	},
+
+	addBook: function(e){
+		e.preventDefault();
+
+		var formData = {};
+		$("#addBook div").children("input").each(function(i,el){
+
+			if($(el).val()!=""){
+				if(el.id === "keywords"){
+					formData[el.id] = [];
+					_.each($(el).val().split( ' ' ),function(keyword){
+						formData[el.id].push({ "keyword": keyword });
+					});
+				}
+				else if(el.id === "releaseDate"){
+					formData[el.id] =$("#releaseDate").datepicker("getDate").getTime();
+				}
+				else{
+					formData[el.id] = $(el).val();
+				}
+			}
+			//Clear input field value
+			$(el).val("");
+		});
+		
+		this.collection.create(formData);
+	},
+});
